e1253eb6f6d08514ef3900cc8bcd51440abaabf1
Vladimir Vozniak <vozniak.vladimir90@gmail.com>
2020-10-23 17:59:24 +0300

    Add some changes in class Quadrangle

--- Decomposition_using_methods/src/Quadrangle.java
+++ Decomposition_using_methods/src/Quadrangle.java
@@ -1,13 +1,25 @@
+import static java.lang.Math.*;
 //Даны числа X, Y, Z, Т — длины сторон четырехугольника.
 //Написать метод(методы) вычисления его площади, если угол между сторонами длиной X и Y— прямой.
 
 public class Quadrangle {
-    private static int side_x = 2;
-    private static int side_y = 4;
-    private static int side_z = 2;
-    private static int side_t = 4;
+    private static double side_x = 2;
+    private static double side_y = 4;
+    private static double side_z = 3.5;
+    private static double side_t = 1;
 
     public static void main(String[] args) {
-        System.out.println("Area of Quadrangle is: " + side_x * side_y);
+        System.out.println(getArea());
+    }
+
+    private static double getArea() {
+        double areaOfQuadrangle;
+        double hypotenuse = sqrt(pow(side_x, 2) + pow(side_y, 2));
+        double areaRightTriangle = (side_x * side_y) / 2;
+        double half_meter = (hypotenuse + side_t + side_z) / 2;
+        double areaSecondTriangle = sqrt(half_meter * (half_meter - side_t) *
+                (half_meter - side_z) * (half_meter - hypotenuse));
+        areaOfQuadrangle = areaRightTriangle + areaSecondTriangle;
+        return areaOfQuadrangle;
     }
 }
